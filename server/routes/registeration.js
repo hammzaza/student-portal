ce7b4a082fbc3203cde13a72b601b6ff6215f9f2
hamza <hamza.ali5542@gmail.com>
2018-08-07 18:49:44 +0500

    router saves

--- server/routes/registeration.js
+++ server/routes/registeration.js
@@ -2,20 +2,23 @@ var Teacher = require('../schema/teacher');
 var randomstring = require("randomstring");
 module.exports = function(app){
     app.post('/register-teacher',function(req,res){
-        teacher = new Teacher();
-        teacher.username = randomstring.generate({length: 7,charset: 'alphabetic'});
-        teacher.password = teacher.generateHash(randomstring.generate({length: 7,charset: 'alphabetic'}));
-        teacher.full_name = req.body.full_name;
-        teacher.teacher = req.body.teacher;
-        teacher.school = req.body.school;
-        teacher.class = req.body.class;
-        
+        teach = new Teacher();
+        teach.username = randomstring.generate({length: 7,charset: 'alphabetic'});
+        teach.password = teach.generateHash(randomstring.generate({length: 7,charset: 'alphabetic'}));
+        teach.full_name = req.body.full_name;
+        teach.teach = req.body.teacher;
+        teach.school = req.body.school;
+        teach.class = req.body.class;
+        Teacher.create(teach, function (err, post) {
+            if (err) {
+                console.log(err);
+                return err;
+            }
+            else{
+                ////////addd message code here
+                res.json(post);
+            }
+             
+         });
     });
 };
-
-// username: String,
-// password: String,
-// full_name: String,
-// teacher: String,
-// school: String,
-// class: String,
