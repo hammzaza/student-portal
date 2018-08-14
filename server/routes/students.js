ad1247fb787ca1bf8f499e1f99de22fbded28f6b
hamza <hamza.ali5542@gmail.com>
2018-08-14 14:05:03 +0500

    left with text message integration

--- server/routes/students.js
+++ server/routes/students.js
@@ -1,8 +1,43 @@
 var Student = require('../schema/student');
-
+var Active = require('../schema/active');
+var Attendence = require('../schema/attendence');
+var HomeWork = require('../schema/homework');
+var StudentScore = require('../schema/student-score');
 module.exports = function(app,passport){
-    app.get('', passport.authenticate('parent-auth', {session: false}), function (req, res) {
-
+    app.get('/parent/get-active', passport.authenticate('parent-auth', { session: false }), function (req, res) {
+        Active.find({
+            student_username: req.user.username}, function (err, result) {
+            res.json({
+                active: result
+            });
+        });
+    });
+    app.get('/parent/get-attendance', passport.authenticate('parent-auth', { session: false }), function (req, res) {
+        Attendence.find({
+            student_username: req.user.username
+        }, function (err, result) {
+            res.json({
+                attendence: result
+            });
+        });
+    });
+    app.get('/parent/get-studentScore', passport.authenticate('parent-auth', { session: false }), function (req, res) {
+        StudentScore.find({
+            student_username: req.user.username
+        }, function (err, result) {
+            res.json({
+                student_score: result
+            });
+        });
+    });
+    app.get('/parent/get-homework', passport.authenticate('parent-auth', { session: false }), function (req, res) {
+        HomeWork.find({
+            student_username: req.user.username
+        }, function (err, result) {
+            res.json({
+                homework: result
+            });
+        });
     });
 
     
